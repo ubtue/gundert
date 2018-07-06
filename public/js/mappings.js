06520ffd3ea36cf706fb8bc69c46aa63877fdb73
Mario Trojan <mario.trojan@googlemail.com>
2018-07-06 17:06:01 +0200

    changes to page+subject structure as discussed with @Gazeller

--- public/js/mappings.js
+++ public/js/mappings.js
@@ -15,12 +15,12 @@
  *
  * subject_ids (GND-Number, translations see ini files):
  * - 4035964-5
- * - 4153493-1 !!! not assigned !!!
+ * - 4153493-1
  * - 4053458-3
  * - 4066724-8
  * - 4021806-5
  * - 4142
- * - 4008240-4 !!! not assigned !!!
+ * - 4008240-4
  * - 4056550-6
  * - 4225695-1
  * - 4010110-1
@@ -33,7 +33,7 @@ var GundertCategoryMappings = {
         // materials
         "printed_all": {"query": {"collection": "52"}, "result": ["date", "title", "subject_ids", "languages"]},
         "manuscripts_all": {"query": {"collection": "53"}, "result": ["date", "title", "subject_ids", "languages"]},
-        "publications": {"query": {"collection": "52+53"}, "result": ["date", "title", "subject_ids", "languages"]},
+        "overall_portfolio": {"query": {"collection": "52+53"}, "result": ["date", "title", "subject_ids", "languages", "collection"]},
 
         // material by language
         "malayalam_print": {"query": {"collection": "52", "languages": "mal"}, "result": ["date", "title", "subject_ids"]},
@@ -44,13 +44,16 @@ var GundertCategoryMappings = {
         "tamil_manuscript": {"query": {"collection": "53", "languages": "tam"}, "result": ["date", "title", "subject_ids"]},
         "telugu_print": {"query": {"collection": "52", "languages": "tel"}, "result": ["date", "title", "subject_ids"]},
         "telugu_manuscript": {"query": {"collection": "53", "languages": "tel"}, "result": ["date", "title", "subject_ids"]},
+        "sanskrit_print": {"query": {"collection": "52", "languages": "san"}, "result": ["date", "title", "subject_ids"]},
+        "sanskrit_manuscript": {"query": {"collection": "53", "languages": "san"}, "result": ["date", "title", "subject_ids"]},
         "german_print": {"query": {"collection": "52", "languages": "ger"}, "result": ["date", "title", "subject_ids"]},
         "german_manuscript": {"query": {"collection": "53", "languages": "ger"}, "result": ["date", "title", "subject_ids"]},
         "english_print": {"query": {"collection": "52", "languages": "eng"}, "result": ["date", "title", "subject_ids"]},
         "english_manuscript": {"query": {"collection": "53", "languages": "eng"}, "result": ["date", "title", "subject_ids"]},
 
         // material by genre
-        "prose": {"query": {"collection": "52+53", "subject_ids": "4035964-5"}, "result": ["date", "title", "subject_ids", "languages", "collection"]},
+        "fiction": {"query": {"collection": "52+53", "subject_ids": "4035964-5"}, "result": ["date", "title", "subject_ids", "languages", "collection"]},
+        "non-fiction": {"query": {"collection": "52+53", "subject_ids": "4153493-1"}, "result": ["date", "title", "subject_ids", "languages", "collection"]},
         "proverbs": {"query": {"collection": "52+53", "subject_ids": "4056550-6"}, "result": ["date", "title", "subject_ids", "languages", "collection"]},
         "grammars_and_dictionaries": {"query": {"collection": "52+53", "subject_ids": "4053458-3+4066724-8+4021806-5"}, "result": ["date", "title", "subject_ids", "languages", "collection"]},
         "religious_works": {"query": {"collection": "52+53", "subject_ids": "4010110-1"}, "result": ["date", "title", "subject_ids", "languages", "collection"]},
